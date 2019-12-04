5d7b537f46a9c7fe12490d4079a8fd29309eb4d0
z3by <zoaby.am@gmail.com>
2019-12-04 16:23:28 +0200

    stop service worker

--- src/main.js
+++ src/main.js
@@ -1,9 +1,8 @@
-import '@babel/polyfill'
-import 'mutationobserver-shim'
+import "@babel/polyfill";
+import "mutationobserver-shim";
 import Vue from "vue";
-import './plugins/bootstrap-vue'
+import "./plugins/bootstrap-vue";
 import App from "./App.vue";
-import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 
