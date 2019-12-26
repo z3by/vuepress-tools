2fbf78566096bde36d9c98f71433f7da913abd1e
z3by <zoaby.am@gmail.com>
2019-12-26 06:50:48 +0200

    add favicons

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -18,7 +18,14 @@ module.exports = {
   head: [
     ['meta', { name: 'theme-color', content: '#3eaf7c' }],
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
-    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ['link', { rel: "apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png" }],
+    ['link', { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon-32x32.png" }],
+    ['link', { rel: "icon", type: "image/png", sizes: "16x16", href: "/favicon-16x16.png" }],
+    ['link', { rel: "manifest", href: "/site.webmanifest" }],
+    ['link', { rel: "mask-icon", href: "/safari-pinned-tab.svg", color: "#5bbad5" }],
+    ['meta', { name: "msapplication-TileColor", content: "#da532c" }],
+    ['meta', { name: "theme-color", content: "#ffffff" }]
   ],
 
   /**
