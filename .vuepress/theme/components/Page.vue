ed37be9f62add7be122bb71b9ae24752b826f168
z3by <zoaby.am@gmail.com>
2020-07-07 17:56:58 +0300

    add ins tags

--- .vuepress/theme/components/Page.vue
+++ .vuepress/theme/components/Page.vue
@@ -1,8 +1,20 @@
 <template>
   <main class="page">
     <slot name="top" />
-
+    <ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-8518970931701152"
+     data-ad-slot="3317068607"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+  
     <Content class="theme-default-content fade-in" />
+    <ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-8518970931701152"
+     data-ad-slot="1315196571"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
     <PageEdit />
 
     <PageNav v-bind="{ sidebarItems }" />
